a0ff188fc99f3f4312e1a70cc020b31f57ef78ae
fabricezamia-ops <fabrice.zamia@gmail.com>
2026-06-08 14:19:43 +0200

    Refactor response handling in callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,24 +1,34 @@
 export default async function handler(req, res) {
   const { code } = req.query;
+  
   const response = await fetch('https://github.com/login/oauth/access_token', {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
+    headers: { 
+      'Content-Type': 'application/json', 
+      'Accept': 'application/json' 
+    },
     body: JSON.stringify({
       client_id: process.env.GITHUB_CLIENT_ID,
       client_secret: process.env.GITHUB_CLIENT_SECRET,
       code
     })
   });
+  
   const data = await response.json();
   const token = data.access_token;
+  
   res.setHeader('Content-Type', 'text/html');
-  res.send(`
-    <script>
-      window.opener.postMessage(
-        'authorization:github:success:${JSON.stringify({ token, provider: 'github' })}',
-        '*'
-      );
-      window.close();
-    </script>
-  `);
+  res.send(`<!DOCTYPE html><html><body><script>
+    (function() {
+      function receiveMessage(e) {
+        console.log("receiveMessage %o", e);
+        window.opener.postMessage(
+          'authorization:github:success:{"token":"${token}","provider":"github"}',
+          e.origin
+        );
+      }
+      window.addEventListener("message", receiveMessage, false);
+      window.opener.postMessage("authorizing:github", "*");
+    })();
+  </script></body></html>`);
 }
